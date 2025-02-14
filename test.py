2e3a73a9228e328485ade17749d1891a4dc4cfe5
KNyathi <khayelihlenyathi07@gmail.com>
2025-02-14 14:05:36 +0300

    completed 7 labs

--- test.py
+++ test.py
@@ -5,6 +5,13 @@ print(cv2.__version__)
 
 #create a numpy array with zeros to use as a blank image
 image = np.zeros((512, 512, 3), np.uint8)
+#image = np.ones((512, 512, 3), np.uint8) * 255  # White image
+#image = np.full((512, 512, 3), 128, np.uint8)  # Gray image
+#image = np.zeros((512, 512), np.uint8)  # Grayscale image
+#image = np.random.randint(0, 256, (512, 512, 3), np.uint8)  # Random color image
+#image = np.zeros((512, 512, 4), np.uint8)  # RGBA image (fourth is for transparency - alpha value)
+
+
 #image = cv.imread("image5.jpg")
 
 # draw a green line on the image
@@ -17,7 +24,7 @@ cv2.rectangle(image, (384, 0), (510, 128), (0,0,255), 3)
 cv2.circle(image, (447, 63), 63, (255, 0,0), -1)
 
 #draw an ellipse
-cv2.ellipse(image,(256,256),(100,50),0,0,180,255,-1)
+cv2.ellipse(image,(256,256),(100,50),0,90,360,(255,255,255),-1)
 
 #draw a polygon
 pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
